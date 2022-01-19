1c04d2004e870b5c806b7c7246c37792d084fd32
“MarcoAOliari” <marcoaoliari@gmail.com>
2022-01-19 18:34:52 -0300

    frontend configurado

---
+++ frontend/src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function TodoForm() {
+    const [input, setInput] = useState('');
+
+    return (
+        <form className="todo-form">
+            <input
+             type="text" 
+             placeholder="Adicione uma Tarefa" value={input}
+             name="text" 
+             className="todo-input" 
+            />
+            <button className="todo-button">Adicionar</button>
+        </form>
+    );
+}
+
+export default TodoForm;
