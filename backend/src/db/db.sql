8283a3e4485f17c0a7d7e911f2b230337326347d
“MarcoAOliari” <marcoaoliari@gmail.com>
2022-01-19 16:24:01 -0300

    tabelas criadas

--- backend/src/db/db.sql
+++ backend/src/db/db.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE todolist;
+
+CREATE TABLE todolists (
+    id SERIAL,
+    title VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE tasks (
+    id SERIAL,
+    title VARCHAR(200) NOT NULL,
+    PRIMARY KEY(id)
+);
