557abdca61db548c107a13dcb46960b3865d9556
“MarcoAOliari” <marcoaoliari@gmail.com>
2022-01-19 21:23:01 -0300

    base frontend

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -1,17 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-function TodoForm() {
-    const [input, setInput] = useState('');
+function TodoForm(props) {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    })
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+
+        setInput('');
+    }
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
 
     return (
-        <form className="todo-form">
-            <input
-             type="text" 
-             placeholder="Adicione uma Tarefa" value={input}
-             name="text" 
-             className="todo-input" 
-            />
-            <button className="todo-button">Adicionar</button>
+        <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+                <>
+                    <input
+                    type="text" 
+                    placeholder="Adicione uma Tarefa" value={input}
+                    name="text" 
+                    className="todo-input" 
+                    onChange={handleChange}
+                    ref={inputRef}
+                    />
+                    <button className="todo-button edit">Atualizar</button>
+                </>
+            ) : (
+                <>
+                    <input
+                    type="text" 
+                    placeholder="Adicione uma Tarefa" value={input}
+                    name="text" 
+                    className="todo-input" 
+                    onChange={handleChange}
+                    ref={inputRef}
+                    />
+                    <button className="todo-button">Adicionar</button>
+                </>
+            )}
         </form>
     );
 }
