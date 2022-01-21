ab0b18d8b715b9fbab9f3af4272e514acedeb06b
“MarcoAOliari” <marcoaoliari@gmail.com>
2022-01-21 15:07:56 -0300

    comentário em rotas

--- backend/src/routes/Task.js
+++ backend/src/routes/Task.js
@@ -3,9 +3,16 @@ const router = express.Router();
 
 const TaskController = require('../controllers/Task.controller');
 
+// Cadastra uma task em uma to-do list
 router.post('/todolist/:id/task', TaskController.store);
+
+// Apaga uma task do banco de dados
 router.delete('/task/:id', TaskController.delete);
+
+// Atualiza o status de uma task
 router.put('/task/:id/status', TaskController.updateStatus);
+
+// Atualiza o texto de uma task
 router.put('/task/:id/title', TaskController.updateTitle);
 
 module.exports = router;
--- backend/src/routes/TodoList.js
+++ backend/src/routes/TodoList.js
@@ -3,10 +3,19 @@ const router = express.Router();
 
 const TodoListController = require('../controllers/TodoList.controller');
 
+// Retorna todas as to-do lists cadastradas
 router.get('/todolist', TodoListController.index);
+
+// Cadastra uma nova to-do list
 router.post('/todolist', TodoListController.store);
+
+// Retorna todas as tasks de uma to-do list
 router.get('/todolist/:id', TodoListController.show);
+
+// Atualiza o título de uma to-do list
 router.put('/todolist/:id', TodoListController.update);
+
+// Apaga uma to-do list do banco de dados
 router.delete('/todolist/:id', TodoListController.delete);
 
 module.exports = router;
